a8bcdead4ce1544c6e7a12300a4586debbb0d5f7
Dylan Parker <dgparker5150@gmail.com>
2018-01-17 21:36:13 -0600

    pretty quick and husky test

--- app/Controllers/main.controller.js
+++ app/Controllers/main.controller.js
@@ -1,6 +1,8 @@
 // A main controller adds a nice base to the app
 // making situations where you need to handle events
 // or share data between views a lot easier.
-function MainController() {}
+function MainController() {
+  var test = "test";
+}
 MainController.$inject = [];
 angular.module("app").controller("MainController", MainController);
